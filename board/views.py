4f8f36ad79faa2cfb42d219b5a8fb105d76c0758
MasakiTanaka-9627 <masa.08279@gmail.com>
2020-03-08 17:00:01 +0900

    board編集機能追加

--- board/views.py
+++ board/views.py
@@ -8,34 +8,51 @@ from django.views.decorators.http import require_POST
 
 # Board
 
+
 def board_createfunc(request):
     if request.method == 'GET':
         return render(request, 'board_create.html')
-            
+
     if request.method == 'POST' and (not request.POST.get('author')):
         post_content = request.POST['content']
         print(post_content)
-        return render(request, 'board_create.html', {'content':post_content})
-        
+        return render(request, 'board_create.html', {'content': post_content})
+
     if request.method == 'POST':
         post_title = request.POST['title']
         post_content = request.POST['content']
         post_author = request.POST['author']
-        board = BoardModel(title=post_title, content=post_content, author=post_author)
+        board = BoardModel(
+            title=post_title, content=post_content, author=post_author)
         board.save()
-        messages.success(request, '記事を作成しました。タイトル:{post.title}')
+        messages.success(request, '記事を作成しました。')
         return redirect('board_list')
+
 
 def board_listfunc(request):
     boards = BoardModel.objects.all().order_by('-created_at')
-    return render(request, 'board_list.html', {'boards':boards})
+    return render(request, 'board_list.html', {'boards': boards})
+
 
 def board_detailfunc(request, pk):
     board = BoardModel.objects.get(pk=pk)
-    return render(request, 'board_detail.html', {'board':board})
+    return render(request, 'board_detail.html', {'board': board})
+
 
 @require_POST
 def board_deletefunc(request, pk):
     board = get_object_or_404(BoardModel, id=pk)
     board.delete()
     return redirect('board_list')
+
+
+def board_editfunc(request, pk):
+    board = get_object_or_404(BoardModel, id=pk)
+
+    if request.method == 'POST':
+        board.title = request.POST.get('title')
+        board.content = request.POST.get('content')
+        board.save()
+        return redirect('board_detail', pk)
+
+    return render(request, 'board_create.html', {'board': board})
