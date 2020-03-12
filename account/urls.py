fe9c0baa1e6bc44c4299294e5d99d7730475a266
MasakiTanaka-9627 <masa.08279@gmail.com>
2020-03-12 22:53:29 +0900

    グラフ描写

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import homefunc, signupfunc, logoutfunc, loginfunc, user_listfunc, user_detailfunc, user_editfunc
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+
+app_name = 'account'
 
 urlpatterns = [
     path('', homefunc, name='home'),    
@@ -10,5 +13,7 @@ urlpatterns = [
     path('login/', loginfunc, name='login'),
     path('userlist/', user_listfunc, name='user_list'),
     path('detail/<int:pk>', user_detailfunc, name='user_detail'),
-    path('edit/<int:pk>', user_editfunc, name='user_edit'),    
+    path('edit/<int:pk>', user_editfunc, name='user_edit'),
+    path('analysis', views.analysis_screen, name='analysis_screen'),
+    path('analysis/plot', views.img_plot, name="img_plot"),
 ]
--- account/views.py
+++ account/views.py
@@ -107,7 +107,6 @@ def analysis_screen(request):
 #画像埋め込み用view
 def img_plot(request):
     # matplotを使って作図する
-    (ex)
     x = [1, 5, 9]
     y = [4, 6, 8]
     ax = plt.subplot()
@@ -116,3 +115,38 @@ def img_plot(request):
     plt.cla()
     response = HttpResponse(png, content_type='image/png')
     return response
+
+def graph2():
+    import matplotlib.pyplot
+    from matplotlib.backends.backend_agg import FigureCanvasAgg
+    import random
+    import string
+    import os
+
+    class TempImage(object):
+
+        def __init__(self, file_name):
+            self.file_name = file_name
+
+        def create_png(self):
+            fig, ax = matplotlib.pyplot.subplots()
+            ax.set_title(u'IMINASHI GRAPH 2')
+            x_ax = range(1, 284)
+            y_ax = [x * random.randint(436, 875) for x in x_ax]
+            ax.plot(x_ax, y_ax)
+
+            canvas = FigureCanvasAgg(fig)
+            canvas.print_figure(self.file_name)
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            os.remove(self.file_name)
+
+    chars = string.digits + string.letters
+    img_name = ''.join(random.choice(chars) for i in xrange(64)) + '.png'
+
+    with TempImage(img_name) as img:
+        img.create_png()
+        return send_file(img_name, mimetype='image/png')
