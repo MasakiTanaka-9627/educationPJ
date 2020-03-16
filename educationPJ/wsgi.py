fd21766d2245cc754299f0e74adc3f35819f988d
MasakiTanaka-9627 <masa.08279@gmail.com>
2020-03-16 22:50:20 +0900

    heroku

--- educationPJ/wsgi.py
+++ educationPJ/wsgi.py
@@ -1,8 +1,16 @@
-import os
+"""
+WSGI config for educationPJ project.
 
-from dj_static import Cling
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
 from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educationPJ.settings")
 
-application = Cling(get_wsgi_application())
