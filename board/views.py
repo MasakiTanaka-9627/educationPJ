0c4ce17122683199aefeb9136a54b3c8a0f800cd
MasakiTanaka-9627 <masa.08279@gmail.com>
2020-03-16 22:13:47 +0900

    CreateImage:Done

--- board/views.py
+++ board/views.py
@@ -10,11 +10,10 @@ from django.views.generic import CreateView
 
 # Board
 
-
 def board_createfunc(request):
     if request.method == 'GET':
         return render(request, 'board_create.html')
-
+    
     if request.method == 'POST':
         post_content = request.POST.get('content')
         post_title = request.POST.get('title')
@@ -23,25 +22,19 @@ def board_createfunc(request):
         if post_title is None:
             return render(request, 'board_create.html', {'content': post_content})
         board = BoardModel(
-            title=post_title, content=post_content, author=user
+            title = post_title, content = post_content, author = user
         )
         board.save()
-
-        
-        post_title = request.POST.get('title')
-        image = request.FILES.get('user')
-        user = User.objects.get(pk=user_id)
-        if post_title is None:
-            return render(request, 'board_create.html', {'content': post_content})
-        board = BoardModel(
-            title=post_title, content=post_content, author=user
+        print(board.pk)
+        image_title = request.POST.get('title')
+        post_image = request.FILES['image']
+        board_image = BoardImage.objects.create(
+            title = image_title, image = post_image
         )
-        board.save()
-
-
+        print(post_image)
+        board_image.save()
         messages.success(request, '記事を作成しました。')
         return redirect('board_list')
-
 
 def board_listfunc(request):
     boards = BoardModel.objects.all().order_by('-created_at')
@@ -49,19 +42,16 @@ def board_listfunc(request):
         board.ans_count = AnsModel.objects.filter(board_id_id=board.id).count()
     return render(request, 'board_list.html', {'boards': boards})
 
-
 def board_detailfunc(request, pk):
     board = BoardModel.objects.get(pk=pk)
     ans_all = AnsModel.objects.filter(board_id_id=pk)
     return render(request, 'board_detail.html', {'board': board, 'ans_all': ans_all})
-
 
 @require_POST
 def board_deletefunc(request, pk):
     board = get_object_or_404(BoardModel, id=pk)
     board.delete()
     return redirect('board_list')
-
 
 def board_editfunc(request, pk):
     board = get_object_or_404(BoardModel, id=pk)
@@ -71,5 +61,3 @@ def board_editfunc(request, pk):
         board.content = request.POST.get('content')
         board.save()
         return redirect('board_detail', pk)
-
- 
